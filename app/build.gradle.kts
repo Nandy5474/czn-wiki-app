2e051b6e85e78393dcb07ff61dc87538a5027c8d
niuy <niuy@epshealth.com>
2026-06-24 17:03:31 +0800

    v2.0.38: 修复5项数据错误 & 补充泰妮布尼雅字段

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.cznwiki.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 37
-        versionName = "2.0.37"
+        versionCode = 38
+        versionName = "2.0.38"
         vectorDrawables {
             useSupportLibrary = true
         }
