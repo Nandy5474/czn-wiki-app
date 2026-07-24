342048b8d3db2546255e21d792ca207f0104ae78
niuy <niuy@epshealth.com>
2026-07-24 18:14:47 +0800

    v2.0.44: 修正绯卡池类型为复刻

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.cznwiki.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 43
-        versionName = "2.0.43"
+        versionCode = 44
+        versionName = "2.0.44"
         vectorDrawables {
             useSupportLibrary = true
         }
